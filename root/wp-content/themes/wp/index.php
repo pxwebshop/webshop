10758006bc37be0c9ea01d3b2290a65a77080d16
dangquy <pxwebshop@gmail.com>
2022-03-16 07:56:36 +0700

    add page top

--- root/wp-content/themes/wp/index.php
+++ root/wp-content/themes/wp/index.php
@@ -2,7 +2,204 @@
 <?php get_header(); ?>
 
 <main class="p-index">
-	index 23434
+	<section class="p-article__sc1 clearfix">
+		<div class="l-container">
+            <div class="p-article__content">
+                <div class="c-list2">
+                    <div class="c-list2__item">
+                        <figure class="c-list2__img">
+                            <img src="https://placehold.jp/220x300.png" alt="">
+                        </figure>
+                        <div class="c-list2__info">
+                            <h3 class="c-list2__title">
+                                <a href="#">arcu dui vivamus arcu felis</a>
+                            </h3>
+                            <div class="c-list2__time">
+                                <i class="fa-solid fa-calendar-days"></i>
+                                <span>14 Tháng Ba, 2022</span>
+                            </div>
+                            <div class="c-list2__author">
+                                <i class="fa-solid fa-user"></i>
+                                <span>Đặng Ngọc Quý</span>
+                            </div>
+                            <div class="c-list2__txt">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eleifend mi in nulla posuere sollicitudin aliquam ultrices. Phasellus vestibulum lorem sed risus ultricies tristique nulla. Urna id volutpat lacus laoreet non curabitur gravida arcu. Magna sit amet purus gravida quis blandit turpis cursus.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="c-list2__item">
+                        <figure class="c-list2__img">
+                            <img src="https://placehold.jp/220x200.png" alt="">
+                        </figure>
+                        <div class="c-list2__info">
+                            <h3 class="c-list2__title">
+                                <a href="#">arcu dui vivamus arcu felis</a>
+                            </h3>
+                            <div class="c-list2__time">
+                                <i class="fa-solid fa-calendar-days"></i>
+                                <span>14 Tháng Ba, 2022</span>
+                            </div>
+                            <div class="c-list2__author">
+                                <i class="fa-solid fa-user"></i>
+                                <span>Rhoshandiatellyneshiaunneveshenk Koyaanisquatsiuth Williams.</span>
+                            </div>
+                            <div class="c-list2__txt">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eleifend mi in nulla posuere sollicitudin aliquam ultrices. Phasellus vestibulum lorem sed risus ultricies tristique nulla. Urna id volutpat lacus laoreet non curabitur gravida arcu. Magna sit amet purus gravida quis blandit turpis cursus.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="c-list2__item">
+                        <figure class="c-list2__img">
+                            <img src="https://placehold.jp/220x200.png" alt="">
+                        </figure>
+                        <div class="c-list2__info">
+                            <h3 class="c-list2__title">
+                                <a href="#">arcu dui vivamus arcu felis</a>
+                            </h3>
+                            <div class="c-list2__time">
+                                <i class="fa-solid fa-calendar-days"></i>
+                                <span>14 Tháng Ba, 2022</span>
+                            </div>
+                            <div class="c-list2__author">
+                                <i class="fa-solid fa-user"></i>
+                                <span>Đặng Ngọc Quý</span>
+                            </div>
+                            <div class="c-list2__txt">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eleifend mi in nulla posuere sollicitudin aliquam ultrices. Phasellus vestibulum lorem sed risus ultricies tristique nulla. Urna id volutpat lacus laoreet non curabitur gravida arcu. Magna sit amet purus gravida quis blandit turpis cursus.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="c-list2__item">
+                        <figure class="c-list2__img">
+                            <img src="https://placehold.jp/220x200.png" alt="">
+                        </figure>
+                        <div class="c-list2__info">
+                            <h3 class="c-list2__title">
+                                <a href="#">arcu dui vivamus arcu felis</a>
+                            </h3>
+                            <div class="c-list2__time">
+                                <i class="fa-solid fa-calendar-days"></i>
+                                <span>14 Tháng Ba, 2022</span>
+                            </div>
+                            <div class="c-list2__author">
+                                <i class="fa-solid fa-user"></i>
+                                <span>Đặng Ngọc Quý</span>
+                            </div>
+                            <div class="c-list2__txt">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eleifend mi in nulla posuere sollicitudin aliquam ultrices. Phasellus vestibulum lorem sed risus ultricies tristique nulla. Urna id volutpat lacus laoreet non curabitur gravida arcu. Magna sit amet purus gravida quis blandit turpis cursus.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="c-list2__item">
+                        <figure class="c-list2__img">
+                            <img src="https://placehold.jp/220x200.png" alt="">
+                        </figure>
+                        <div class="c-list2__info">
+                            <h3 class="c-list2__title">
+                                <a href="#">arcu dui vivamus arcu felis</a>
+                            </h3>
+                            <div class="c-list2__time">
+                                <i class="fa-solid fa-calendar-days"></i>
+                                <span>14 Tháng Ba, 2022</span>
+                            </div>
+                            <div class="c-list2__author">
+                                <i class="fa-solid fa-user"></i>
+                                <span>Đặng Ngọc Quý</span>
+                            </div>
+                            <div class="c-list2__txt">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eleifend mi in nulla posuere sollicitudin aliquam ultrices. Phasellus vestibulum lorem sed risus ultricies tristique nulla. Urna id volutpat lacus laoreet non curabitur gravida arcu. Magna sit amet purus gravida quis blandit turpis cursus.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="c-list2__item">
+                        <figure class="c-list2__img">
+                            <img src="https://placehold.jp/220x200.png" alt="">
+                        </figure>
+                        <div class="c-list2__info">
+                            <h3 class="c-list2__title">
+                                <a href="#">arcu dui vivamus arcu felis</a>
+                            </h3>
+                            <div class="c-list2__time">
+                                <i class="fa-solid fa-calendar-days"></i>
+                                <span>14 Tháng Ba, 2022</span>
+                            </div>
+                            <div class="c-list2__author">
+                                <i class="fa-solid fa-user"></i>
+                                <span>Đặng Ngọc Quý</span>
+                            </div>
+                            <div class="c-list2__txt">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eleifend mi in nulla posuere sollicitudin aliquam ultrices. Phasellus vestibulum lorem sed risus ultricies tristique nulla. Urna id volutpat lacus laoreet non curabitur gravida arcu. Magna sit amet purus gravida quis blandit turpis cursus.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="p-article__articleOther">
+                <div class="c-block1">
+                    <h3 class="c-block1__heading">Bài viết khác</h3>
+                    <div class="c-block1__inner">
+                        <div class="c-block1__item">
+                            <figure class="c-block1__img">
+                                <img src="https://placehold.jp/120x100.png" alt="">
+                            </figure>
+                            <div class="c-block1__info">
+                                <h5 class="c-block1__title">Tuyển dụng nhân viên khinh doanh</h5>
+                                <p class="c-block1__txt">Nhân viên có kiến thức về sale, và biết về excel. Để hộ trợ trong công việc</p>
+                            </div>
+                        </div>
+                        <div class="c-block1__item">
+                            <figure class="c-block1__img">
+                                <img src="https://placehold.jp/120x100.png" alt="">
+                            </figure>
+                            <div class="c-block1__info">
+                                <h5 class="c-block1__title">Tuyển dụng nhân viên khinh doanh</h5>
+                                <p class="c-block1__txt">Nhân viên có kiến thức về sale, và biết về excel. Để hộ trợ trong công việc</p>
+                            </div>
+                        </div>
+                        <div class="c-block1__item">
+                            <figure class="c-block1__img">
+                                <img src="https://placehold.jp/120x100.png" alt="">
+                            </figure>
+                            <div class="c-block1__info">
+                                <h5 class="c-block1__title">Tuyển dụng nhân viên khinh doanh</h5>
+                                <p class="c-block1__txt">Nhân viên có kiến thức về sale, và biết về excel. Để hộ trợ trong công việc</p>
+                            </div>
+                        </div>
+                        <div class="c-block1__item">
+                            <figure class="c-block1__img">
+                                <img src="https://placehold.jp/120x100.png" alt="">
+                            </figure>
+                            <div class="c-block1__info">
+                                <h5 class="c-block1__title">Tuyển dụng nhân viên khinh doanh</h5>
+                                <p class="c-block1__txt">Nhân viên có kiến thức về sale, và biết về excel. Để hộ trợ trong công việc</p>
+                            </div>
+                        </div>
+
+                        <div class="c-block1__item">
+                            <figure class="c-block1__img">
+                                <img src="https://placehold.jp/120x100.png" alt="">
+                            </figure>
+                            <div class="c-block1__info">
+                                <h5 class="c-block1__title">Tuyển dụng nhân viên khinh doanh</h5>
+                                <p class="c-block1__txt">Nhân viên có kiến thức về sale, và biết về excel. Để hộ trợ trong công việc</p>
+                            </div>
+                        </div>
+                        <div class="c-block1__item">
+                            <figure class="c-block1__img">
+                                <img src="https://placehold.jp/120x100.png" alt="">
+                            </figure>
+                            <div class="c-block1__info">
+                                <h5 class="c-block1__title">Tuyển dụng nhân viên khinh doanh</h5>
+                                <p class="c-block1__txt">Nhân viên có kiến thức về sale, và biết về excel. Để hộ trợ trong công việc</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 </main>
 
 <?php get_footer(); ?>
