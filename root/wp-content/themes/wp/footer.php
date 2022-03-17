e852f70c60bf580013eb7c422e01a2b95375bad1
dangquy <pxwebshop@gmail.com>
2022-03-17 08:02:14 +0700

    change path in file foooter

--- root/wp-content/themes/wp/footer.php
+++ root/wp-content/themes/wp/footer.php
@@ -7,11 +7,11 @@
 
 <?php wp_footer(); ?>
 
-<script src="<?php bloginfo( 'template_url' ); ?>/assets/js/jquery-3.6.0.min.js"></script>
-<script src="<?php bloginfo( 'template_url' ); ?>/assets/js/jquery-migrate-3.3.2.min.js"></script>
-<script src="<?php bloginfo( 'template_url' ); ?>/assets/js/footer-fixed.min.js"></script>
+<script src="<?php '/assets/js/jquery-3.6.0.min.js"></script>
+<script src="<?php '/assets/js/jquery-migrate-3.3.2.min.js"></script>
+<script src="<?php '/assets/js/footer-fixed.min.js"></script>
 
-<script src="<?php bloginfo( 'template_url' ); ?>/assets/js/common.js"></script>
+<script src="<?php '/assets/js/common.js"></script>
 
 </body>
 </html>
