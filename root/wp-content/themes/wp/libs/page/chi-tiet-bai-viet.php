73e5c42e04ecc4c01f2f3d6da18635f89f10f4c8
congminhnguyen <minhcongnguyen11@gmail.com>
2022-03-23 11:17:55 +0700

    a

---
+++ root/wp-content/themes/wp/libs/page/chi-tiet-bai-viet.php
@@ -0,0 +1,38 @@
+<?php $page_id = 'chi-tiet-bai-viet'; ?>
+<?php get_header(); ?>
+<main class="p-detail">
+    <div class="c-detail">
+        <div class="c-deatail__heading">
+            <figure>
+                <img src="https://designmodo.com/wp-content/uploads/2020/01/Valentines-Day-Email-Newsletter-1920x1080.jpg" alt="">
+            </figure>
+        </div>
+        <div class="l-container">
+            <div class="c-detail__article">
+                <div class="c-detail__share">
+                    <span><i class="fa-solid fa-share-nodes"></i>Share</span>
+                    <ul>
+                        <li>
+                            <i class="fa-brands fa-facebook">Share</i>
+                        </li>
+                        <li>
+                            <i class="fa-brands fa-twitter">Tweet</i>
+                        </li>
+                        <li>
+                            <i class="fa-brands fa-linkedin">Share</i>
+                        </li>
+                        <li>
+                            <i class="fa-brands fa-pinterest">Pin It</i>
+                        </li>
+                    </ul>
+                </div>
+                <div class="c-detail__content">
+                    <h1 class="c-detail__title1">
+                        Examples of Valentine’s Day Email Newsletters and Subject Lines
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<?php get_footer(); ?>
