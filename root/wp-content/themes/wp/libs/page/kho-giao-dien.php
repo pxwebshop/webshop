c4bb5d5ecf65b9c0862a385d34e6fa6991047967
congminhnguyen <minhcongnguyen11@gmail.com>
2022-03-24 10:15:52 +0700

    layout interface stock

---
+++ root/wp-content/themes/wp/libs/page/kho-giao-dien.php
@@ -0,0 +1,112 @@
+<?php $page_id = 'kho-giao-dien'; ?>
+<?php get_header(); ?>
+
+<main class="p-stock">
+    <div class="c-stock">
+        <div class="c-page__heading" id="particles-js">
+            <div class="c-page__title">
+                <div>
+                    <h2 class="c-title1 c-title1--color1">Kho giao diện</h2>
+                    <div class="c-page__url">
+                        <a href="/" class="c-page__link">Trang chủ</a>
+                        <a href="/kho-giao-dien">/ Kho giao diện</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="l-container">
+            <ul class="c-stock__tag">
+                <li class="c-stock__tagItem"><a href="#">Landing Page</a></li>
+                <li class="c-stock__tagItem"><a href="#">Website bán hàng</a></li>
+                <li class="c-stock__tagItem"><a href="#">Website giáo dục</a></li>
+                <li class="c-stock__tagItem"><a href="#">Website giới thiệu công ty</a></li>
+                <li class="c-stock__tagItem"><a href="#">Website giới thiệu dịch vụ</a></li>
+                <li class="c-stock__tagItem"><a href="#">Website giới thiệu dự án</a></li>
+                <li class="c-stock__tagItem"><a href="#">Website giới thiệu sản phẩm</a></li>
+                <li class="c-stock__tagItem"><a href="#">Website Kiến trúc - Nội thất</a></li>
+                <li class="c-stock__tagItem"><a href="#">Website giao vặt</a></li>
+                <li class="c-stock__tagItem"><a href="#">Website thẩm mỹ, spa</a></li>
+                <li class="c-stock__tagItem"><a href="#">Website bất động sản</a></li>
+                <li class="c-stock__tagItem"><a href="#">Website du lịch</a></li>
+                <li class="c-stock__tagItem"><a href="#">Website tin tức</a></li>
+                <li class="c-stock__tagItem"><a href="#">Danh mục khác</a></li>
+            </ul>
+            <div class="c-list11">
+                <div class="c-list11__wrap">
+                    <div class="c-list11__item">
+                        <div class="c-list11__content">
+                            <figure class="c-list11__img">
+                                <img src="https://websiteviet.vn/wp-content/uploads/2020/10/art.websiteviet.vn_.png" alt="">
+                            </figure>
+                            <div class="c-list11__link">
+                                <a href="#" class="c-list11__demo">Demo</a>
+                                <a href="#" class="c-list11__detail">Chi tiết</a>
+                            </div>
+                        </div>
+                        <div class="c-list11__info">
+                            <p class="c-list11__tag">Web giáo dục</p>
+                            <p class="c-list11__name">Mẫu web dạy nhạc cụ</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="c-list11__wrap">
+                    <div class="c-list11__item">
+                        <div class="c-list11__content">
+                            <figure class="c-list11__img">
+                                <img src="https://websiteviet.vn/wp-content/uploads/2020/10/art.websiteviet.vn_.png" alt="">
+                            </figure>
+                            <div class="c-list11__link">
+                                <a href="#" class="c-list11__demo">Demo</a>
+                                <a href="#" class="c-list11__detail">Chi tiết</a>
+                            </div>
+                        </div>
+                        <div class="c-list11__info">
+                            <p class="c-list11__tag">Web giáo dục</p>
+                            <p class="c-list11__name">Mẫu web dạy nhạc cụ</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="c-list11__wrap">
+                    <div class="c-list11__item">
+                        <div class="c-list11__content">
+                            <figure class="c-list11__img">
+                                <img src="https://websiteviet.vn/wp-content/uploads/2020/10/art.websiteviet.vn_.png" alt="">
+                            </figure>
+                            <div class="c-list11__link">
+                                <a href="#" class="c-list11__demo">Demo</a>
+                                <a href="#" class="c-list11__detail">Chi tiết</a>
+                            </div>
+                        </div>
+                        <div class="c-list11__info">
+                            <p class="c-list11__tag">Web giáo dục</p>
+                            <p class="c-list11__name">Mẫu web dạy nhạc cụ</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="c-list11__wrap">
+                    <div class="c-list11__item">
+                        <div class="c-list11__content">
+                            <figure class="c-list11__img">
+                                <img src="https://websiteviet.vn/wp-content/uploads/2020/10/art.websiteviet.vn_.png" alt="">
+                            </figure>
+                            <div class="c-list11__link">
+                                <a href="#" class="c-list11__demo">Demo</a>
+                                <a href="#" class="c-list11__detail">Chi tiết</a>
+                            </div>
+                        </div>
+                        <div class="c-list11__info">
+                            <p class="c-list11__tag">Web giáo dục</p>
+                            <p class="c-list11__name">Mẫu web dạy nhạc cụ</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php get_footer(); ?>
