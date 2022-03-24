fbb1eed3b641564f356ef39a3483fdc578434f83
congminhnguyen <minhcongnguyen11@gmail.com>
2022-03-24 18:42:22 +0700

    demo page

--- root/wp-content/themes/wp/libs/page/kho-giao-dien.php
+++ root/wp-content/themes/wp/libs/page/kho-giao-dien.php
@@ -54,16 +54,16 @@
                     <div class="c-list11__item">
                         <div class="c-list11__content">
                             <figure class="c-list11__img">
-                                <img src="https://websiteviet.vn/wp-content/uploads/2020/10/art.websiteviet.vn_.png" alt="">
+                                <img src="https://websiteviet.vn/wp-content/uploads/2020/10/edu5.websiteviet.vn_.png" alt="">
                             </figure>
                             <div class="c-list11__link">
-                                <a href="#" class="c-list11__demo">Demo</a>
+                                <a href="/assets/demo-page/websitedaytienganh/index.htm" class="c-list11__demo">Demo</a>
                                 <a href="#" class="c-list11__detail">Chi tiết</a>
                             </div>
                         </div>
                         <div class="c-list11__info">
                             <p class="c-list11__tag">Web giáo dục</p>
-                            <p class="c-list11__name">Mẫu web dạy nhạc cụ</p>
+                            <p class="c-list11__name">Mẫu web dạy tiếng anh</p>
                         </div>
                     </div>
 
@@ -72,16 +72,16 @@
                     <div class="c-list11__item">
                         <div class="c-list11__content">
                             <figure class="c-list11__img">
-                                <img src="https://websiteviet.vn/wp-content/uploads/2020/10/art.websiteviet.vn_.png" alt="">
+                                <img src="https://websiteviet.vn/wp-content/uploads/2021/06/macshopnbs.jpeg" alt="">
                             </figure>
                             <div class="c-list11__link">
-                                <a href="#" class="c-list11__demo">Demo</a>
+                                <a href="/assets/demo-page/websitebandocongngheapple/index.htm" class="c-list11__demo">Demo</a>
                                 <a href="#" class="c-list11__detail">Chi tiết</a>
                             </div>
                         </div>
                         <div class="c-list11__info">
-                            <p class="c-list11__tag">Web giáo dục</p>
-                            <p class="c-list11__name">Mẫu web dạy nhạc cụ</p>
+                            <p class="c-list11__tag">Web bán hàng</p>
+                            <p class="c-list11__name">Mẫu web bán đồ công nghệ apple</p>
                         </div>
                     </div>
 
@@ -90,19 +90,222 @@
                     <div class="c-list11__item">
                         <div class="c-list11__content">
                             <figure class="c-list11__img">
-                                <img src="https://websiteviet.vn/wp-content/uploads/2020/10/art.websiteviet.vn_.png" alt="">
+                                <img src="https://websiteviet.vn/wp-content/uploads/2020/10/marketing.websiteviet.vn_.png" alt="">
                             </figure>
                             <div class="c-list11__link">
-                                <a href="#" class="c-list11__demo">Demo</a>
+                                <a href="/assets/demo-page/websitemarketing/index.htm" class="c-list11__demo">Demo</a>
+                                <a href="#" class="c-list11__detail">Chi tiết</a>
+                            </div>
+                        </div>
+                        <div class="c-list11__info">
+                            <p class="c-list11__tag">Web giới thiệu sản phẩm</p>
+                            <p class="c-list11__name">Mẫu web marketing</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="c-list11__wrap">
+                    <div class="c-list11__item">
+                        <div class="c-list11__content">
+                            <figure class="c-list11__img">
+                                <img src="https://websiteviet.vn/wp-content/uploads/2021/06/nhakhoa3nbw.jpeg" alt="">
+                            </figure>
+                            <div class="c-list11__link">
+                                <a href="/assets/demo-page/websitenhakhoa/index.htm" class="c-list11__demo">Demo</a>
+                                <a href="#" class="c-list11__detail">Chi tiết</a>
+                            </div>
+                        </div>
+                        <div class="c-list11__info">
+                            <p class="c-list11__tag">Web giới thiệu dịch vụ</p>
+                            <p class="c-list11__name">Mẫu web trung tâm nha khoa</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="c-list11__wrap">
+                    <div class="c-list11__item">
+                        <div class="c-list11__content">
+                            <figure class="c-list11__img">
+                                <img src="https://websiteviet.vn/wp-content/uploads/2020/10/pianocenter.websiteviet.vn_.png" alt="">
+                            </figure>
+                            <div class="c-list11__link">
+                                <a href="/assets/demo-page/websitehocdan/index.htm" class="c-list11__demo">Demo</a>
                                 <a href="#" class="c-list11__detail">Chi tiết</a>
                             </div>
                         </div>
                         <div class="c-list11__info">
                             <p class="c-list11__tag">Web giáo dục</p>
-                            <p class="c-list11__name">Mẫu web dạy nhạc cụ</p>
+                            <p class="c-list11__name">Mẫu web đào tạo học đàn</p>
                         </div>
                     </div>
-
+                </div>
+                <div class="c-list11__wrap">
+                    <div class="c-list11__item">
+                        <div class="c-list11__content">
+                            <figure class="c-list11__img">
+                                <img src="https://web4s.vn/uploads/plugin/themes/47/1602576952-1897699859-th-i-trang-02.jpg" alt="">
+                            </figure>
+                            <div class="c-list11__link">
+                                <a href="/assets/demo-page/websitebanquanao/view-demo/fs-05.html" class="c-list11__demo">Demo</a>
+                                <a href="#" class="c-list11__detail">Chi tiết</a>
+                            </div>
+                        </div>
+                        <div class="c-list11__info">
+                            <p class="c-list11__tag">Web bán hàng</p>
+                            <p class="c-list11__name">Mẫu web bán quần áo</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="c-list11__wrap">
+                    <div class="c-list11__item">
+                        <div class="c-list11__content">
+                            <figure class="c-list11__img">
+                                <img src="https://mauwebsite.vn/wp-content/uploads/2019/03/FireShot-Capture-168-Leisure-mauweb.monamedia.net_-450x992.jpg" alt="">
+                            </figure>
+                            <div class="c-list11__link">
+                                <a href="/assets/demo-page/websitedichvukhachsan/leisure/index.htm" class="c-list11__demo">Demo</a>
+                                <a href="#" class="c-list11__detail">Chi tiết</a>
+                            </div>
+                        </div>
+                        <div class="c-list11__info">
+                            <p class="c-list11__tag">Web dịch vụ</p>
+                            <p class="c-list11__name">Mẫu web dịch vụ khách sạn</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="c-list11__wrap">
+                    <div class="c-list11__item">
+                        <div class="c-list11__content">
+                            <figure class="c-list11__img">
+                                <img src="https://mauwebsite.vn/wp-content/uploads/2018/11/du-lich-kham-pha-1-450x1635.jpg" alt="">
+                            </figure>
+                            <div class="c-list11__link">
+                                <a href="/assets/demo-page/websitedulichkhampha/index.htm" class="c-list11__demo">Demo</a>
+                                <a href="#" class="c-list11__detail">Chi tiết</a>
+                            </div>
+                        </div>
+                        <div class="c-list11__info">
+                            <p class="c-list11__tag">Web du lịch</p>
+                            <p class="c-list11__name">Mẫu web du lịch khám phá</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="c-list11__wrap">
+                    <div class="c-list11__item">
+                        <div class="c-list11__content">
+                            <figure class="c-list11__img">
+                                <img src="https://websiteviet.vn/wp-content/uploads/2021/06/bds-scaled.jpg" alt="">
+                            </figure>
+                            <div class="c-list11__link">
+                                <a href="/assets/demo-page/websitebatdongsan/index.htm" class="c-list11__demo">Demo</a>
+                                <a href="#" class="c-list11__detail">Chi tiết</a>
+                            </div>
+                        </div>
+                        <div class="c-list11__info">
+                            <p class="c-list11__tag">Web giới thiệu dự án</p>
+                            <p class="c-list11__name">Mẫu web bất động sản</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="c-list11__wrap">
+                    <div class="c-list11__item">
+                        <div class="c-list11__content">
+                            <figure class="c-list11__img">
+                                <img src="https://websiteviet.vn/wp-content/uploads/2021/06/vothuatnbw-scaled.jpeg" alt="">
+                            </figure>
+                            <div class="c-list11__link">
+                                <a href="/assets/demo-page/landingpage-vothuat/index.htm" class="c-list11__demo">Demo</a>
+                                <a href="#" class="c-list11__detail">Chi tiết</a>
+                            </div>
+                        </div>
+                        <div class="c-list11__info">
+                            <p class="c-list11__tag">Landing page</p>
+                            <p class="c-list11__name">Landing page - Võ thuật</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="c-list11__wrap">
+                    <div class="c-list11__item">
+                        <div class="c-list11__content">
+                            <figure class="c-list11__img">
+                                <img src="https://websiteviet.vn/wp-content/uploads/2022/01/Crypto-%E2%80%93-Crypto.png" alt="">
+                            </figure>
+                            <div class="c-list11__link">
+                                <a href="/assets/demo-page/landingpage-crypto/index.htm" class="c-list11__demo">Demo</a>
+                                <a href="#" class="c-list11__detail">Chi tiết</a>
+                            </div>
+                        </div>
+                        <div class="c-list11__info">
+                            <p class="c-list11__tag">Landing page</p>
+                            <p class="c-list11__name">Landing page - Crypto</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="c-list11__wrap">
+                    <div class="c-list11__item">
+                        <div class="c-list11__content">
+                            <figure class="c-list11__img">
+                                <img src="https://websiteviet.vn/wp-content/uploads/2021/06/duocphamnbs.jpeg" alt="">
+                            </figure>
+                            <div class="c-list11__link">
+                                <a href="/assets/demo-page/websitecongtyduoc/index.htm" class="c-list11__demo">Demo</a>
+                                <a href="#" class="c-list11__detail">Chi tiết</a>
+                            </div>
+                        </div>
+                        <div class="c-list11__info">
+                            <p class="c-list11__tag">Web giới thiệu công ty</p>
+                            <p class="c-list11__name">Mẫu web giới thiệu công ty dược</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="c-list11__wrap">
+                    <div class="c-list11__item">
+                        <div class="c-list11__content">
+                            <figure class="c-list11__img">
+                                <img src="https://websiteviet.vn/wp-content/uploads/2020/08/kientruc.png" alt="">
+                            </figure>
+                            <div class="c-list11__link">
+                                <a href="/assets/demo-page/websitekientruc/index.htm" class="c-list11__demo">Demo</a>
+                                <a href="#" class="c-list11__detail">Chi tiết</a>
+                            </div>
+                        </div>
+                        <div class="c-list11__info">
+                            <p class="c-list11__tag">Web kiến trúc - nội thất</p>
+                            <p class="c-list11__name">Mẫu web kiến trúc</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="c-list11__wrap">
+                    <div class="c-list11__item">
+                        <div class="c-list11__content">
+                            <figure class="c-list11__img">
+                                <img src="https://websiteviet.vn/wp-content/uploads/2020/08/noithat.websiteviet.vn_.png" alt="">
+                            </figure>
+                            <div class="c-list11__link">
+                                <a href="/assets/demo-page/websitenoithat/index.htm" class="c-list11__demo">Demo</a>
+                                <a href="#" class="c-list11__detail">Chi tiết</a>
+                            </div>
+                        </div>
+                        <div class="c-list11__info">
+                            <p class="c-list11__tag">Web kiến trúc - nội thất</p>
+                            <p class="c-list11__name">Mẫu web nội thất</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="c-list11__wrap">
+                    <div class="c-list11__item">
+                        <div class="c-list11__content">
+                            <figure class="c-list11__img">
+                                <img src="https://websiteviet.vn/wp-content/uploads/2021/10/mypham-scaled.jpg" alt="">
+                            </figure>
+                            <div class="c-list11__link">
+                                <a href="/assets/demo-page/websitecuahangmypham/index.htm" class="c-list11__demo">Demo</a>
+                                <a href="#" class="c-list11__detail">Chi tiết</a>
+                            </div>
+                        </div>
+                        <div class="c-list11__info">
+                            <p class="c-list11__tag">Web thẩm mỹ viện, spa</p>
+                            <p class="c-list11__name">Mẫu web cửa hàng mỹ phẩm</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
