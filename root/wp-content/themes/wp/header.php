f19c5206aad92e9207c705538610ba046f838dae
dangquy <pxwebshop@gmail.com>
2022-03-31 23:29:40 +0700

    add

--- root/wp-content/themes/wp/header.php
+++ root/wp-content/themes/wp/header.php
@@ -17,11 +17,6 @@
 
 	<link rel='stylesheet' href='/assets/css/animate.min.css'>
 	<script src='/assets/js/wow.min.js'></script>
-
-	<!-- font noto sans lao -->
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Lao:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 </head>
 
 <body>
