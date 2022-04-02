092a36c5a818d09c4bea6ba7ebd9cab322bbc807
dangquy <pxwebshop@gmail.com>
2022-04-02 10:51:34 +0700

    customer domain

--- root/wp-content/themes/wp/functions/other.php
+++ root/wp-content/themes/wp/functions/other.php
@@ -1,6 +1,22 @@
 <?php
-
 /* ===============================================================================
   その他
 =============================================================================== */
 
+add_filter( 'site_url', 'wpse_381006_custom_site_url', 10, 1 );
+
+function wpse_381006_custom_site_url( $url ){
+    if( is_admin() ) // you probably don't want this in admin side
+        return $url;
+
+    $domain = [0 => 'webshop.local'];
+    $host = $_SERVER['HTTP_HOST'];
+    
+    $not_local = array_search($host, $domain);
+    if (empty($not_local)) {
+      return '/index.php'. $_SERVER['REQUEST_URI'];
+    }
+
+
+    return $_SERVER['REQUEST_URI'];
+}
