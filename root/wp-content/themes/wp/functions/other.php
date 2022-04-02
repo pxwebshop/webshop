4bdeec7f600ecefa2ea3f6f040f86fcce57955aa
dangquy <pxwebshop@gmail.com>
2022-04-02 10:55:10 +0700

    fix domain

--- root/wp-content/themes/wp/functions/other.php
+++ root/wp-content/themes/wp/functions/other.php
@@ -9,14 +9,12 @@ function wpse_381006_custom_site_url( $url ){
     if( is_admin() ) // you probably don't want this in admin side
         return $url;
 
-    $domain = [0 => 'webshop.local'];
+    $domain_local = 'webshop.local';
     $host = $_SERVER['HTTP_HOST'];
     
-    $not_local = array_search($host, $domain);
-    if (empty($not_local)) {
+    // is server
+    if (stripos($domain_local, $host) !== false) {
       return '/index.php'. $_SERVER['REQUEST_URI'];
-    }
-
-
+  
     return $_SERVER['REQUEST_URI'];
 }
