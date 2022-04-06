bdd93c2b84c29305e7388efee60bc7b79370666c
dangquy <pxwebshop@gmail.com>
2022-04-06 23:04:00 +0700

    add

--- root/wp-content/themes/wp/libs/page/lien-he.php
+++ root/wp-content/themes/wp/libs/page/lien-he.php
@@ -14,45 +14,6 @@
                 </div>
             </div>
         </div>
-        <div class="c-contact__map">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.21284151982!2d106.71968101535644!3d10.79500416180106!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317527c2f8f30911%3A0x36ac5073f8c91acd!2sLandmark%2081!5e0!3m2!1sen!2s!4v1647486094027!5m2!1sen!2s" allowfullscreen="" loading="lazy"></iframe>
-        </div>
-        <div class="l-container">
-            <div class="c-contact__content">
-                <div class="c-contact__info">
-                    <div class="c-contact__phone">
-                        <i class="fa-solid fa-phone"></i>
-                        <span class="c-contact__txt"> &nbsp020 28266650</span>
-                    </div>
-                    <div class="c-contact__email">
-                        <i class="fa-solid fa-envelope"></i>
-                        <span class="c-contact__txt"> &nbsppxwebshop@gmail.com</span>
-                    </div>
-                    <div class="c-contact__address1">
-                        <i class="fa-solid fa-location-dot"></i>
-                        <span class="c-contact__txt"> &nbspບ້ານ ຂາມງອຍ,​ ເມືອງ ໄຊເສດຖາ, ນະຄອນຫຼວງວຽງຈັນ</span>
-                    </div>
-                </div>
-                <div class="c-contact__form">
-                    <form action="#">
-                        <div class="c-contact__group">
-                            <label for="name">ຊື່ ແລະ ນາມສະກຸນ</label>
-                            <input type="text" name="yourname" id="name">
-                        </div>
-                        <div class="c-contact__group">
-                            <label for="email">ອີເມວ</label>
-                            <input type="text" id="email">
-                        </div>
-                        <div class="c-contact__group">
-                            <label for="phone">ເບີໂທ</label>
-                            <input type="text" id="phone">
-                        </div>
-                        <div class="c-contact__group">
-                            <label for="message">ຂໍ້ຄວາມ</label>
-                            <textarea rows="10" name="" id="message"></textarea>
-                        </div>
-                        <button class="c-contact__form__btn" type="submit">ສົ່ງ</button>
-                    </form>
         <div class="grid-bg ba-grid anim">
             <div class="c-contact__map">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7589.420135974364!2d102.66638249405541!3d17.992221101233906!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3124679cd1397e6f%3A0x6820293abbbefccf!2z4LqaLiDgu4HguqrguofguqrgurDguqvguqfgu4jgurLguocg4Lqu4LuI4Lqt4LqhIDYs4LuE4LqK4LuA4Lqq4LqU4LqW4LqyLOC6meC6sOC6hOC6reC6meC6q-C6vOC6p-C6h-C6p-C6veC6h-C6iOC6seC6mQ!5e0!3m2!1sen!2s!4v1649131023954!5m2!1sen!2s" allowfullscreen="" loading="lazy"></iframe>
