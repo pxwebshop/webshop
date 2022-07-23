bc78c88830ad8b4e017dadd7f5da0c88213e9c88
dangquy <dangquy95php@gmail.com>
2022-07-23 09:21:23 +0700

    website-spa-beauty

--- root/wp-content/themes/wp/libs/page/website-spa-beauty.php
+++ root/wp-content/themes/wp/libs/page/website-spa-beauty.php
@@ -10,7 +10,7 @@
                     <div class="c-page__url">
                         <a href="/" class="c-page__link">ໜ້າຫຼັກ</a>
                         <a href="/repository-interface">/ ຕົວຢ່າງເວັບໄຊ</a>
-                        <a href="/website-tham-my-spa">/ ເວັບໄຊຄວາມງາມ - Spa</a>
+                        <a href="/website-spa-beauty">/ ເວັບໄຊຄວາມງາມ - Spa</a>
                     </div>
                 </div>
             </div>
